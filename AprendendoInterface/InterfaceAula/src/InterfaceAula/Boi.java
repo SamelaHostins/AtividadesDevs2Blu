4fdfcbb600a84c7c2ac08f4d220f8c0bb3d43935
Sâmela Hostins <samela@hostins.com.br>
2021-12-13 21:00:40 -0300

    Add files via upload

---
+++ AprendendoInterface/InterfaceAula/src/InterfaceAula/Boi.java
@@ -0,0 +1,18 @@
+package InterfaceAula;
+
+public class Boi implements Animal {
+
+	public String emitirSom() {
+		String som;
+		som = "muuu";
+		return som;
+	}
+
+	public String tipoAlimento() {
+		String tipoAlimento;
+		tipoAlimento = "Capim";
+		return tipoAlimento;
+		
+	}
+	
+}
