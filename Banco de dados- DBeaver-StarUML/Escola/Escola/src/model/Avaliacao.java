500cd99cd77fb2b4fc77f2f71193109c4f17c972
Sâmela Hostins <samela@hostins.com.br>
2022-02-07 19:50:45 -0300

    Update Avaliacao.java

--- Banco de dados- DBeaver-StarUML/Escola/Escola/src/model/Avaliacao.java
+++ Banco de dados- DBeaver-StarUML/Escola/Escola/src/model/Avaliacao.java
@@ -14,19 +14,18 @@ import controller.Conexao;
 
 public class Avaliacao {
 
-	private int codAvaliacao;
+	private int codDisciplina;
 	private int codAluno;
 	private int nrAvaliacao;
 	private double vlrNota;
 	
 	
-	
-	public int getCodAvaliacao() {
-		return codAvaliacao;
+	public int getCodDisciplina() {
+		return codDisciplina;
 	}
 
-	public void setCodAvaliacao(int codAvaliacao) {
-		this.codAvaliacao = codAvaliacao;
+	public void setCodDisciplina(int codDisciplina) {
+		this.codDisciplina = codDisciplina;
 	}
 
 	public int getCodAluno() {
@@ -54,205 +53,192 @@ public class Avaliacao {
 	}
 
 	//Testa a conexão no main
-			public void testeConexao() {
-				if(Conexao.conectar() != null) {
-					System.out.println("conectado");
-				}else {
-					System.out.println("erro ao conectar ao banco");
-				}
-		}
+		public void testeConexao() {
+			if(Conexao.conectar() != null) {
+				System.out.println("conectado");
+			}else {
+				System.out.println("erro ao conectar ao banco");
+			}
+	 }
+		
+		
+		//Metodo que salva uma Avaliação no banco
+		public boolean salvarAvaliacao() {
+			Connection con = Conexao.conectar();	
+			String sql = "insert into avaliacao(codaluno, coddisciplina, nravaliacao, vlrnota) values(?,?,?,?)";
 			
-	/*
-	//Metodo que salva um aluno no banco
-			public boolean salvarAvaliacao() {
-				Connection con = Conexao.conectar();	
-				String sql = "insert into avaliacao(nomaluno, nommae, nompai, datanasc) values(?,?,?,?)";
-				
-				try {
-					PreparedStatement stm = con.prepareStatement(sql);
-					stm.setString(1, this.getNomAluno());
-					stm.setString(2, this.getNomMae());
-					stm.setString(3, this.getNomPai());
-					stm.setDate(4, this.getDataNasc());
-					stm.execute();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				finally {
-					Conexao.fecharConexao();
-				}
-				return true;
-			}		
-			
-			//Metodo para incluir um aluno que salvará no main
-			public void incluirAluno() {
-				Aluno alu = new Aluno();
-				
-				String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
-				alu.setNomAluno(nome);
+			try {
+				PreparedStatement stm = con.prepareStatement(sql);
+				stm.setInt(1, this.getCodAluno());
+				stm.setInt(2, this.getCodDisciplina());
+				stm.setInt(3, this.getNrAvaliacao());
+				stm.setDouble(4, this.getVlrNota());
+				stm.execute();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			finally {
+				Conexao.fecharConexao();
+			}
+			return true;
+		}		
+		
+		//Metodo para incluir uma avaliação que salvará no main
+		public void incluirAvaliacao() {
+			Avaliacao ma = new Avaliacao();			
 
-				String nomeMae = JOptionPane.showInputDialog("Qual o nome de sua mãe?");
-				alu.setNomMae(nomeMae);
-				
-				String nomePai = JOptionPane.showInputDialog("Informe o nome do pai");
-				alu.setNomPai(nomePai);
-				
-				String data = JOptionPane.showInputDialog("Informe a data de nascimento");
-				alu.setDataNasc(Date.valueOf(data));				
-				
-				if(alu.salvarAluno()) {
-					System.out.println("Aluno salvo com sucesso");
-				}
-			}
+			String codAlu = JOptionPane.showInputDialog("Qual o código do Aluno?");
+			ma.setCodAluno(Integer.valueOf(codAlu));
 			
-			//Consulta se o aluno existe no banco
-					public Aluno consultaPeloCodigo(int pcodaluno) {
-						Aluno aluno = new Aluno();
-						String sql = "SELECT * FROM ALUNO WHERE codaluno = " + pcodaluno;
-						try {
-							PreparedStatement stm = Conexao.conectar().prepareStatement(sql);
-							ResultSet rs = stm.executeQuery();
-							
-							if(rs.next()) {
-								aluno.setCodAluno(rs.getInt("codaluno"));	
-								aluno.setNomAluno(rs.getString("nomaluno"));
-								aluno.setNomMae(rs.getString("nommae"));
-								aluno.setNomPai(rs.getString("nompai"));
-								aluno.setDataNasc(rs.getDate("datanasc"));
-							}
-						} catch (SQLException e) {
-							e.printStackTrace();
-						}
-						finally {
-							Conexao.fecharConexao();
-						}
-						return aluno;		
-					}
-					
-					//Para consultar um aluno no main
-					public void consultarAluno() {
-						Aluno a = new Aluno().consultaPeloCodigo(3);
-						System.out.println(a.getCodAluno());
-						System.out.println(a.getNomAluno());
-						System.out.println(a.getNomMae());
-						System.out.println(a.getNomPai());
-						System.out.println(a.getDataNasc());
-						System.out.println("");
-					}				
+			String codDis = JOptionPane.showInputDialog("Qual o código da Disciplina?");
+			ma.setCodDisciplina(Integer.valueOf(codDis));
+			
+			String nrAva = JOptionPane.showInputDialog("Informe o número da avaliação");
+			ma.setNrAvaliacao(Integer.valueOf(nrAva));
+			
+			String vlNota = JOptionPane.showInputDialog("Informe o valor da nota");
+			ma.setVlrNota(Double.valueOf(vlNota));
 		
-					//Criando o arraylist do banco
-					public List<Aluno> consultaTodos() {
-						List<Aluno> alunos = new ArrayList<>();
-						String sql = "select * from aluno";
-						try {
-							PreparedStatement stm = Conexao.conectar().prepareStatement(sql);
-							//Resultset retorna os dados do banco
-							ResultSet rs = stm.executeQuery();
-							while(rs.next()) {
-								Aluno aluno = new Aluno();
-								aluno.setCodAluno(rs.getInt("codAluno"));	
-								aluno.setNomAluno(rs.getString("nomaluno"));
-								aluno.setNomMae(rs.getString("nommae"));
-								aluno.setNomPai(rs.getString("nompai"));
-								aluno.setDataNasc(rs.getDate("datanasc"));
-								alunos.add(aluno);
-							}
-							
-						} catch (SQLException e) {
-							e.printStackTrace();
-						}		
-						return alunos;
-					}	
-					
-					//Método para ler a lista no main
-					public void lerTodos() {
-					List<Aluno> listaAluno = new Aluno().consultaTodos();
-					for(Aluno c : listaAluno) {
-						System.out.println(c.getCodAluno());
-						System.out.println(c.getNomAluno());
-						System.out.println(c.getNomMae());
-						System.out.println(c.getNomPai());
-						System.out.println(c.getDataNasc());
-						System.out.println("");
-					}
-				}
-					
-					
-			//Método que altera(edita) no banco
-			public boolean alterarAluno() {
-				Connection con = Conexao.conectar();	
-				String sql = "update aluno set nomaluno=?, nommae =?, nompai =?, datanasc =?";
-				sql += " where codaluno = ?";
-				try {
-					PreparedStatement stm = con.prepareStatement(sql);
-					stm.setString(1, this.getNomAluno());
-					stm.setString(2, this.getNomMae());
-					stm.setString(3, this.getNomPai());
-					stm.setDate(4, this.getDataNasc());
-					stm.setInt(5, this.getCodAluno());//Agora estará alterando o codigo, fzd com que ele mude por isso precisa adicioná-lo
-					stm.execute();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				finally {
-					Conexao.fecharConexao();
-				}
-				return true;
-			}
 			
-			//Metodo para alterar um aluno a partir de uma nova variável no main
-			public void alterandoAluno() {
-				Aluno alu = new Aluno();
-				Aluno a = new Aluno();
-				a = alu.consultaPeloCodigo(4);
-				String nomMae = a.getNomMae();
-				
-				nomMae = "Beatriz";
-				alu.setNomMae(nomMae);
-				if(alu.alterarAluno()) {
-					System.out.println("Aluno alterado com sucesso");
-				}else {
-					System.out.println("Problema ao alterar o aluno");
-				}
+			if(ma.salvarAvaliacao()) {
+				System.out.println("Avaliação salva com sucesso");
 			}
-		
-			//Metodo que exclui um Auno no banco
-			public boolean excluirAluno(int codaluno) {
-				String sql = "delete from aluno where codaluno = ?";
-				try {
-					PreparedStatement stm = Conexao.conectar().prepareStatement(sql);
-					stm.setInt(1,codaluno);
-					stm.execute();		
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				finally {
-					Conexao.fecharConexao();
-				}
-				return true;		
-			}
-			
-			//Metodo para excluir no main
-			public void chamaExcluir() {
-				Aluno aluno = new Aluno();
-				if(aluno.excluirAluno(1)) {
-					System.out.println("excluído com sucesso");
-				}
-			}		
-		
-			//Metodo para ler a data de nascimento
-					public void pegarDataDeNascimento() {
-						Aluno alu = new Aluno();
-						String dataNascimento = "2000-02-01";
-						alu.setDataNasc(Date.valueOf(dataNascimento));
-						alu.mostrarData();
-						System.out.println("Data nascimento informada "+ dataNascimento);
-					}
-			
-			//ToString para mostrar a data		
-		    public String mostrarData() {
-			   return "Aluno [dataNasc=" + dataNasc + "]";
 		}
-		*/
 		
+		//Consulta se a avaliação existe no banco
+				public Avaliacao consultaPeloCodigo(int pcod, int pcod2, int pcod3) {
+					Avaliacao di = new Avaliacao();
+					String sql = "SELECT * FROM AVALIACAO WHERE codaluno = " + pcod + 
+							" AND coddisciplina = " + pcod2 + " AND nravaliacao = " + pcod3;
+					try {
+						PreparedStatement stm = Conexao.conectar().prepareStatement(sql);
+						ResultSet rs = stm.executeQuery();
+						
+						if(rs.next()) {
+							di.setCodAluno(rs.getInt("codaluno"));
+							di.setCodDisciplina(rs.getInt("coddisciplina"));	
+							di.setNrAvaliacao(rs.getInt("nravaliacao"));
+							di.setVlrNota(rs.getDouble("vlrnota"));
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					finally {
+						Conexao.fecharConexao();
+					}
+					return di;		
+				}
+				
+				//Para consultar uma disciplina no main
+				public void consultarAvaliacao() {
+					Avaliacao ma = new Avaliacao().consultaPeloCodigo(0,2,1);
+					System.out.println(ma.getCodAluno());
+					System.out.println(ma.getCodDisciplina());					
+					System.out.println(ma.getNrAvaliacao());
+					System.out.println(ma.getVlrNota());
+					System.out.println("");
+				}				
+
+				//Criando o arraylist do banco
+				public List<Avaliacao> consultaTodos() {
+					List<Avaliacao> avaliacoes = new ArrayList<>();
+					String sql = "select * from avaliacao";
+					try {
+						PreparedStatement stm = Conexao.conectar().prepareStatement(sql);
+						//Resultset retorna os dados do banco
+						ResultSet rs = stm.executeQuery();
+						while(rs.next()) {
+							Avaliacao di = new Avaliacao();								
+							di.setCodAluno(rs.getInt("codaluno"));
+							di.setCodDisciplina(rs.getInt("coddisciplina"));
+							di.setNrAvaliacao(rs.getInt("nravaliacao"));
+							di.setVlrNota(rs.getDouble("vlrnota"));
+							avaliacoes.add(di);
+						}
+						
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}	
+					finally {
+						Conexao.fecharConexao();
+					}
+					return avaliacoes;
+				}	
+				
+				//Método para ler a lista no main
+				public void lerTodosAva() { 
+				List<Avaliacao> listaAvaliacao = new Avaliacao().consultaTodos();
+				for(Avaliacao c : listaAvaliacao) {
+					System.out.println(c.getCodAluno());
+					System.out.println(c.getCodDisciplina());
+					System.out.println(c.getNrAvaliacao());
+					System.out.println(c.getVlrNota());
+					System.out.println("");
+				}
+			}
+				
+				
+		//Método que altera(edita) no banco
+		public boolean alterarAvaliacao() {
+			Connection con = Conexao.conectar();	
+			String sql = "update avaliacao set codaluno =?, coddisciplina=?, nravaliacao =?, vlrnota =?";
+			sql += " where codaluno =? and coddisciplina =? and nravaliacao =?";
+			try {
+				PreparedStatement stm = con.prepareStatement(sql);
+				stm.setInt(1, this.getCodAluno());
+				stm.setInt(2, this.getCodDisciplina());
+				stm.setInt(3, this.getNrAvaliacao());
+				stm.setDouble(4, this.getVlrNota());
+				stm.setInt(5, this.getCodAluno());
+				stm.setInt(6, this.getCodDisciplina());
+				stm.setInt(7, this.getNrAvaliacao());
+				stm.execute();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			finally {
+				Conexao.fecharConexao();
+			}
+			return true;
+		}
+		
+		//Metodo para alterar uma avaliacao a partir de uma nova variável no main
+		public void alterandoAvaliacao() {
+			Avaliacao di = new Avaliacao();
+			di = di.consultaPeloCodigo(0,2,1);
+			
+			di.setVlrNota(7.2);
+			if(di.alterarAvaliacao()) {
+				System.out.println("Avaliação alterada com sucesso");
+			}else {
+				System.out.println("Avaliação ao alterar a Disciplina");
+			}
+		}
+
+		//Metodo que exclui uma matricula no banco
+		public boolean excluirAvaliacao( int codaluno, int coddisciplina, int nravaliacao) {
+			String sql = "delete from avaliacao where codaluno =? and coddisciplina = ? and nravaliacao =?";
+			try {
+				PreparedStatement stm = Conexao.conectar().prepareStatement(sql);
+				stm.setInt(1,codaluno);
+				stm.setInt(2,coddisciplina);
+				stm.setInt(3,nravaliacao);
+				stm.execute();		
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			finally {
+				Conexao.fecharConexao();
+			}
+			return true;		
+		}
+		
+		//Metodo para excluir no main
+		public void chamaExcluir() {
+			Avaliacao ma = new Avaliacao();
+			if(ma.excluirAvaliacao(0,2,1)) {
+				System.out.println("excluído com sucesso");
+			}
+		}	
+
 	}
